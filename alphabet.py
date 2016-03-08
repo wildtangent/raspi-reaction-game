726fb266064b7f0f4fb2bc7f149dab585ee8ede0
Joe Connor <joe@authomedia.co.uk>
2016-03-08 14:09:49 +0800

    add final step, fix issues with step 6

---
+++ alphabet.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+# letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p']
+# for letter in letters:
+#   print letter
+
+
+# names = ['Joe', 'Lucy', 'Desmond', 'Derek', 'Eric']
+# for name in names:
+#   print name
+
+# pins = [1, 5, 12, 18]
+# for pinNumber in pins:
+#   print "Pin number: " + str(pinNumber)
+
--- reaction-step7.py
+++ reaction-step7.py
@@ -10,23 +10,36 @@ GPIO.setwarnings(False)
 
 # Set pin assignments
 led = 4
+buzzer = 18
 rightButton = 14
 leftButton = 15
+leftScoreLeds = [10,9,11]
+rightScoreLeds = [18,22,23]
+
+# Set up game variables
+leftScore = 0
+rightScore = 0
+rounds = 0
+maxRounds = 3
 
 # Input player names
 leftName = input("Player 1 enter your name: ")
 rightName = input("Player 2 enter your name: ")
 names = [leftName, rightName]
 
-leftScore = 0
-rightScore = 0
-rounds = 0
-maxRounds = 3
-
 # Setup GPIO pins
 GPIO.setup(led, GPIO.OUT)
+GPIO.setup(buzzer, GPIO.OUT)
 GPIO.setup(rightButton, GPIO.IN, GPIO.PUD_UP)
 GPIO.setup(leftButton, GPIO.IN, GPIO.PUD_UP)
+
+# Setup Score LEDs
+for i in leftScoreLeds:
+  GPIO.setup(i, GPIO.OUT)
+  GPIO.output(i, 0)
+for i in rightScoreLeds:
+  GPIO.setup(i, GPIO.OUT)
+  GPIO.output(i, 0)
 
 while True:
 
@@ -44,25 +57,31 @@ while True:
 
     # Turn LED on then wait for a random time
     GPIO.output(led, 1)
-    
-    # Generate a random time
-    randTime = random.uniform(2,10)
+    GPIO.output(buzzer, 0)
 
-    #  Sleep for random amount of time
+    # Generate a random time
+    randTime = random.uniform(5,10)
+
+    # Sleep for random amount of time
     time.sleep(randTime)
 
     # Turn off. First person to press wins!
     GPIO.output(led, 0)
+    GPIO.output(buzzer,1)
 
     while True:
       # Loop waiting for first player to press
       if GPIO.input(leftButton) == False:
         print(names[0] + " won!")
         leftScore += 1
-        break
+        GPIO.output(buzzer,0)
+        GPIO.output(leftScoreLeds[leftScore-1], 1)
+        break 
       if GPIO.input(rightButton) == False:
         print(names[1] + " won!")
         rightScore += 1
+        GPIO.output(buzzer,0)
+        GPIO.output(rightScoreLeds[rightScore-1], 1)
         break
 
     # Ready for next game...?
@@ -78,5 +97,7 @@ else:
   winner = names[1]
 
 print("Winner:" + winner)
+
+time.sleep(10)
 
 GPIO.cleanup()
